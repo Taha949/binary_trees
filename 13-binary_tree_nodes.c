9783cbc4bcc26630885bdf67e18d18f1487a15b3
Taha949 <maatataha@gmail.com>
2024-05-09 08:33:00 -0700

    c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts
+ *
+ * Return: number or NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree
+ *
+ * Return: size orNULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree
+ *
+ * Return: number or NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
